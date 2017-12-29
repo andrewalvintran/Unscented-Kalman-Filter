953c0f25323d4e48f6dfd0079f68d907a3ce1b9e
Andrew Tran <andrewalvintran@gmail.com>
2017-12-29 01:43:28 -0800

    Predict sigma points

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -46,6 +46,9 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
   //DO NOT MODIFY measurement noise values above these are provided by the sensor manufacturer.
+
+  // measurement dimension for r, phi, and r_dot
+  n_z_ = 3;
   
   n_x_ = 5;
   n_aug_ = 7;
@@ -83,11 +86,60 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  */
 void UKF::Prediction(double delta_t) {
   /**
-  TODO:
-
-  Complete this function! Estimate the object's location. Modify the state
+  Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  VectorXd x_aug = VectorXd(n_aug_);
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2*n_aug_ + 1);
+  
+  // augmented mean state
+  x_aug << x_, 0.0, 0.0;
+
+  // augmented covariance
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(5, 5) = P_;
+  P_aug(5, 5) = std_a_ * std_a_;
+  P_aug(6, 6) = std_yawdd_ * std_yawdd_;
+
+  // square root matrix
+  MatrixXd L = P_aug.llt().matrixL();
+
+  // creating augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+  for (int i = 0; i < n_aug_; i++) {
+    Xsig_aug.col(i+1) = x_aug + sqrt(n_aug_ + lambda_)*L.col(i);
+    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(n_aug_ + lambda_)*L.col(i);
+  }
+
+  double delta_t_sq = delta_t * delta_t;
+
+  // predict sigma points
+  for (int i = 0; i < n_aug_*2 + 1; i++) {
+    double p_x = Xsig_aug(0, i);
+    double p_y = Xsig_aug(1, i);
+    double v = Xsig_aug(2, i);
+    double yaw = Xsig_aug(3, i);
+    double yaw_dot = Xsig_aug(4, i);
+    double nu_a = Xsig_aug(5, i);
+    double nu_yawdd = Xsig_aug(6, i);
+    
+    double px_p, py_p;
+    
+    if (fabs(yaw_dot) <= 0.001) {
+        px_p = p_x + v * delta_t_sq * cos(yaw);
+        py_p = p_y + v * delta_t_sq * sin(yaw);
+    } else {
+        px_p = p_x + v/yaw_dot * (sin(yaw + yaw_dot*delta_t_sq) - sin(yaw));
+        py_p = p_y + v/yaw_dot * (-cos(yaw + yaw_dot*delta_t_sq) + cos(yaw));
+    }
+    
+    Xsig_pred_(0, i) = px_p + 0.5*(delta_t_sq)*cos(yaw)*nu_a;
+    Xsig_pred_(1, i) = py_p + 0.5*(delta_t_sq)*sin(yaw)*nu_a;
+    Xsig_pred_(2, i) = v + delta_t_sq * nu_a;
+    Xsig_pred_(3, i) = yaw + yaw_dot*delta_t_sq + 0.5*(delta_t_sq * delta_t_sq) * nu_yawdd;
+    Xsig_pred_(4, i) = yaw_dot + delta_t_sq*nu_yawdd;
+  }
 }
 
 /**
